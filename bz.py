ec23a7136fd3390e68fd38a810b3cf3ea41795e4
keyi <yike921012@gmail.com>
2016-03-30 01:00:01 -0700

    Initial commit

--- bz.py
+++ bz.py
@@ -11,10 +11,14 @@ path = os.path.join(path, 'baozou')
 if not os.path.exists(path):
     os.mkdir(path)
 
-page_sum = 10
+page_sum = 5
 url = "http://baozoumanhua.com/gif/month/page/"
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'
+                         'AppleWebKit/537.11'
+                         '(KHTML,like Gecko)'
+                         'Chrome/23.0.1271.64 Safari/537.11',
+           'Accept': 'text/html,application/xhtml+xml,'
+                     'application/xml;q=0.9,*/*;q=0.8',
            'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
            'Accept-Encoding': 'none',
            'Accept-Language': 'en-US,en;q=0.8',
@@ -28,18 +32,16 @@ for count in range(page_sum):
         url=url + str(count + 1),
         headers=headers
     )
-    print(req.full_url)
+    print("url: ", req.full_url)
     content = urllib.request.urlopen(req).read()
-    soup = bs4.BeautifulSoup(content, "html.parser")
+    soup = bs4.BeautifulSoup(content, "lxml")
     img_content = soup.findAll('img', src=True, style="width:460px;")
     url_list = [img['src'] for img in img_content]
-    #  title_list = [img['alt'] for img in img_content]
-    print(len(url_list))
+    print("File number in this page: %s" % (len(url_list)))
     for i in range(url_list.__len__()):
         try:
             imgurl = url_list[i]
             imgname = str(imgnum).zfill(4)
-            #  imgname = title_list[i] if title_list[i] !="" else str(imgnum).zfill(4)
             filename = path + os.sep + imgname + ".gif"
             imgnum += 1
             print(filename)
