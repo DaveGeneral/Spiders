1a1fbe798bb0ff16a1d6c9a6164364704ab3a875
keyi <yike921012@gmail.com>
2016-03-30 05:26:16 -0700

    Initial commit

--- douban/rq.py
+++ douban/rq.py
@@ -25,23 +25,31 @@ class DouBanSpider(object):
 
     def find_title(self, my_page):
         temp_data = []
-        soup_title = bs4.BeautifulSoup(my_page, "lxml")
-        movie_title = soup_title.select(".title")
-        soup_rating = bs4.BeautifulSoup(my_page, "lxml")
-        movie_rating = soup_rating.select(".rating_num")
+        soup = bs4.BeautifulSoup(my_page, "lxml")
+        movie_title = soup.select(".title")
+        movie_rating = soup.select(".rating_num")
+        movie_rank = soup.select(".pic em")
+        movie_review = soup.select(".star span")
+        movie_img = soup.select(".pic a img")
+        movie_review, temp_data = temp_data, movie_review
+        for i in range(len(temp_data)):
+            if(i + 1) % 4 == 0:
+                movie_review.append(temp_data[i])
         count = 0
+        temp_data = []
         movie_title, temp_data = temp_data, movie_title
         for x in temp_data:
             if x.string.find('/') == -1:
                 count += 1
                 movie_title.append(x)
-        print(count)
         for i in range(count):
-            print(movie_title[i].string, movie_rating[i].string)
+            print(movie_rank[i].string, movie_title[
+                  i].string, movie_rating[i].string,
+                  movie_review[i].string, movie_img[i]['src'])
         self.datas.extend(temp_data)
 
-    def start_spider(self, pageNum):
-        while self.page <= pageNum:
+    def start_spider(self, pagenum):
+        while self.page <= pagenum:
             my_page = self.get_page(self.page)
             self.find_title(my_page)
             self.page += 1
