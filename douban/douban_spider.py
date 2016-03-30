08512f01841d000acebf4e7946e7a9a7a7e4eba7
keyi <yike921012@gmail.com>
2016-03-30 05:27:10 -0700

    Initial commit

--- douban/douban_spider.py
+++ douban/douban_spider.py
@@ -1,20 +1,9 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-import re
-import urllib.request
-import urllib.error
-import urllib.parse
+import bs4
+
+import requests
 
 
 class DouBanSpider(object):
-
-    """
-    Attributes:
-        page: 用于表示当前所处的抓取页面
-        cur_url: 用于表示当前争取抓取页面的url
-        datas: 存储处理好的抓取到的电影名称
-        _top_num: 用于记录当前的top号码
-    """
 
     def __init__(self):
         self.page = 1
@@ -27,56 +16,40 @@ class DouBanSpider(object):
         print("豆瓣电影爬虫准备就绪, 准备爬取数据...")
 
     def get_page(self, cur_page):
-        """
-
-        根据当前页码爬取网页HTML
-
-        Args:
-            cur_page: 表示当前所抓取的网站页码
-
-        Returns:
-            返回抓取到整个页面的HTML(unicode编码)
-
-        Raises:
-            URLError:url引发的异常
-        """
-        url = self.cur_url
         try:
-            my_page = urllib.request.urlopen(url.format(
-                page=(cur_page - 1) * 25)).read().decode("utf-8")
-        except urllib.error.URLError as e:
-            if hasattr(e, "code"):
-                print("The server couldn't fulfill the request.")
-                print("Error code: %s" % e.code)
-            elif hasattr(e, "reason"):
-                print("Failed to reach a server.")
-                print("Reason: %s" % e.reason)
+            my_page = requests.get(
+                "https://movie.douban.com/top250?start=225&filter=").text
+        except Exception:
+            print("Error happens! Please check your requests.")
         return my_page
 
     def find_title(self, my_page):
-        """
-
-        通过返回的整个网页HTML, 正则匹配前100的电影名称
-
-
-        Args:
-            my_page: 传入页面的HTML文本用于正则匹配
-        """
         temp_data = []
-        movie_items = re.findall(
-            r'<span.*?class="title">(.*?)</span>', my_page, re.S)
-        for index, item in enumerate(movie_items):
-            if item.find("&nbsp") == -1:
-                temp_data.append("Top" + str(self._top_num) + " " + item)
-                self._top_num += 1
+        soup = bs4.BeautifulSoup(my_page, "lxml")
+        movie_title = soup.select(".title")
+        movie_rating = soup.select(".rating_num")
+        movie_rank = soup.select(".pic em")
+        movie_review = soup.select(".star span")
+        movie_img = soup.select(".pic a img")
+        movie_review, temp_data = temp_data, movie_review
+        for i in range(len(temp_data)):
+            if(i + 1) % 4 == 0:
+                movie_review.append(temp_data[i])
+        count = 0
+        temp_data = []
+        movie_title, temp_data = temp_data, movie_title
+        for x in temp_data:
+            if x.string.find('/') == -1:
+                count += 1
+                movie_title.append(x)
+        for i in range(count):
+            print(movie_rank[i].string, movie_title[
+                  i].string, movie_rating[i].string,
+                  movie_review[i].string, movie_img[i]['src'])
         self.datas.extend(temp_data)
 
-    def start_spider(self):
-        """
-
-        爬虫入口, 并控制爬虫抓取页面的范围
-        """
-        while self.page <= 4:
+    def start_spider(self, pagenum):
+        while self.page <= pagenum:
             my_page = self.get_page(self.page)
             self.find_title(my_page)
             self.page += 1
@@ -92,9 +65,7 @@ def main():
         ###############################
     """)
     my_spider = DouBanSpider()
-    my_spider.start_spider()
-    for item in my_spider.datas:
-        print(item)
+    my_spider.start_spider(1)
     print("豆瓣爬虫爬取结束...")
 
 if __name__ == '__main__':
