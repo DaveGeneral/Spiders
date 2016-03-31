8479b5195365b03f44676b54e73dff77cceea3a5
keyi <yike921012@gmail.com>
2016-03-30 18:50:07 -0700

    Initial commit

---
+++ douban/db.py
@@ -0,0 +1,15 @@
+import MySQLdb
+
+conn = MySQLdb.connect(host="localhost", user="spider",
+                       passwd="pwd123456", port=3306, db='Movie', charset="utf8")
+cur = conn.cursor()
+cur.execute("DROP TABLE IF EXISTS Top250")
+sql = """CREATE TABLE Top250(
+         Rank INT NOT NULL,
+         Name VARCHAR(50) NOT NULL,
+         Rating FLOAT NOT NULL,
+         Comment INT NOT NULL,
+         ImgUrl VARCHAR(100) NOT NULL)"""
+cur.execute(sql)
+cur.close()
+conn.close()
