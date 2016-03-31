6163bee010ac52c42899164148df16fba64d1dcd
keyi <yike921012@gmail.com>
2016-03-31 02:14:07 -0700

    Initial commit

--- douban/combine.py
+++ douban/combine.py
@@ -1,6 +1,8 @@
 import bs4
 
-import MySQLdb
+from collections import OrderedDict
+
+import json
 
 import requests
 
@@ -56,7 +58,10 @@ class DouBanSpider(object):
         imgurl = self.get_imgurl(soup)
         count = len(title)
         for i in range(count):
-            print(rank[i], title[i], rating[i], comment[i], imgurl[i])
+            dic = OrderedDict([("Rank:", rank[i]), ("Title:", title[i]),
+                               ("Rating:", rating[i]), ("Comment", comment[i]),
+                               ("Image Url:", imgurl[i])])
+            print(json.dumps(dic, ensure_ascii=False, indent=4))
         self.datas.extend(temp_data)
 
     def start_spider(self, pagenum):
@@ -64,50 +69,6 @@ class DouBanSpider(object):
             my_page = self.get_page(self.page)
             self.get_content(my_page)
             self.page += 1
-
-class DoubanDB(object):
-
-
-    def __init__(self):
-
-
-        conn = MySQLdb.connect(host="localhost", user="spider",
-                       passwd="pwd123456", port=3306,
-                       db='Movie', charset="utf8")
-cur = conn.cursor()
-cur.execute("DROP TABLE IF EXISTS Top250")
-sql = """CREATE TABLE Top250(
-         Rank INT NOT NULL,
-         Name VARCHAR(100) NOT NULL,
-         Rating FLOAT NOT NULL,
-         Comment VARCHAR(100) NOT NULL,
-         ImgUrl VARCHAR(100) NOT NULL)"""
-
-cur.execute(sql)
-sql = """INSERT INTO Top250(
-Rank, Name, Rating, Comment, ImgUrl)
-VALUES ("10", 'taowang', '8.6', '201424 人评价', "www.google.com");"""
-try:
-    cur.execute(sql)
-    conn.commit()
-except:
-    # Rollback in case there is any error
-    conn.rollback()
-
-try:
-    cur.execute("SELECT * FROM Top250;")
-    lines = cur.fetchall()
-    for row in lines:
-        rank = row[0]
-        name = row[1]
-        rating = row[2]
-        comment = row[3]
-        imgurl = row[4]
-        print(rank, name, rating, comment, imgurl)
-except:
-    print("Error: unable to fecth data")
-cur.close()
-conn.close()
 
 
 def main():
@@ -119,8 +80,8 @@ def main():
     """)
     print("Douban Movie Crawler Begins\n")
     my_spider = DouBanSpider()
-    my_spider.start_spider(10)
-    print("Douban Movie Crawler Ends.\n")
+    my_spider.start_spider(1)
+    print("\nDouban Movie Crawler Ends.\n")
 
 if __name__ == '__main__':
     main()
