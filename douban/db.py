cf5eef29b7bbaeadf9a226d059d8f368a4659397
keyi <yike921012@gmail.com>
2016-03-31 15:43:17 -0700

    Initial commit

--- douban/db.py
+++ douban/db.py
@@ -1,46 +1,54 @@
 import MySQLdb
 
-class DoubanDB(object):
 
+class DoubanDB(object):
 
     def __init__(self):
         pass
 
+    def table_create(self, cursor):
+        cursor.execute("DROP TABLE IF EXISTS Douban")
+        sql = """CREATE TABLE Douban(
+                 Rank INT NOT NULL,
+                 Name VARCHAR(100) NOT NULL,
+                 Rating FLOAT NOT NULL,
+                 Comment VARCHAR(100) NOT NULL,
+                 ImgUrl VARCHAR(100) NOT NULL)"""
+        cursor.execute(sql)
+        return
 
-conn = MySQLdb.connect(host="localhost", user="spider",
-                       passwd="pwd123456", port=3306,
-                       db='Movie', charset="utf8")
-cur = conn.cursor()
-cur.execute("DROP TABLE IF EXISTS Douban")
-sql = """CREATE TABLE Douban(
-         Rank INT NOT NULL,
-         Name VARCHAR(100) NOT NULL,
-         Rating FLOAT NOT NULL,
-         Comment VARCHAR(100) NOT NULL,
-         ImgUrl VARCHAR(100) NOT NULL)"""
+    def table_insert(self, conn, cursor):
+        sql = """INSERT INTO Douban(
+        Rank, Name, Rating, Comment, ImgUrl)
+        VALUES ("10", 'taowang', '8.6', '201424 人评价', "www.google.com");"""
+        try:
+            cursor.execute(sql)
+            conn.commit()
+        except:
+            # Rollback in case there is any error
+            conn.rollback()
 
-cur.execute(sql)
-sql = """INSERT INTO Douban(
-Rank, Name, Rating, Comment, ImgUrl)
-VALUES ("10", 'taowang', '8.6', '201424 人评价', "www.google.com");"""
-try:
-    cur.execute(sql)
-    conn.commit()
-except:
-    # Rollback in case there is any error
-    conn.rollback()
+    def table_select(self, cursor):
+        try:
+            cursor.execute("SELECT * FROM Douban;")
+            lines = cursor.fetchall()
+            for row in lines:
+                rank = row[0]
+                name = row[1]
+                rating = row[2]
+                comment = row[3]
+                imgurl = row[4]
+                print(rank, name, rating, comment, imgurl)
+        except:
+            print("Error: unable to fecth data")
 
-try:
-    cur.execute("SELECT * FROM Douban;")
-    lines = cur.fetchall()
-    for row in lines:
-        rank = row[0]
-        name = row[1]
-        rating = row[2]
-        comment = row[3]
-        imgurl = row[4]
-        print(rank, name, rating, comment, imgurl)
-except:
-    print("Error: unable to fecth data")
-cur.close()
-conn.close()
+    def start_db(self, datas):
+        my_conn = MySQLdb.connect(host="localhost", user="spider",
+                                  passwd="pwd123456", port=3306,
+                                  db='Movie', charset="utf8")
+        my_cursor = my_conn.cursor()
+        self.table_create(my_cursor)
+        self.table_insert(my_conn, my_cursor)
+        self.table_select(my_cursor)
+        my_cursor.close()
+        conn.close()
