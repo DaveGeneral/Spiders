e4005dbe3c55c75c8e9888389ad430fa09c06d46
keyi <yike921012@gmail.com>
2016-03-31 23:09:52 -0700

    Initial commit

--- douban/example.py
+++ douban/example.py
@@ -1,9 +1,8 @@
 import json
 from collections import OrderedDict
 with open('output.json') as data_file:
-    data = json.load(data_file)
-    mydata = json.dumps(data, indent=4, ensure_ascii=False,
-                        object_pairs_hook=OrderedDict)
+    data = json.load(data_file, object_pairs_hook=OrderedDict)
+    mydata = json.dumps(data, indent=4, ensure_ascii=False)
 print(len(data))
 print(data['5']['Comment'])
 print(mydata)
