7459d0e7f92405296b1e5372469e79ee684db932
keyi <yike921012@gmail.com>
2016-04-03 01:35:57 -0700

    Initial commit

--- baozou/multi_thread.py
+++ baozou/multi_thread.py
@@ -2,13 +2,15 @@
 # -*- coding:utf-8 -*-
 
 import bs4
-import collections
 import os
-import re
+import queue
 import requests
 import shutil
 import warnings
 
+
+Q_share = queue.Queue()
+thread_num = 10  # the speed shows little increase beyond this number
 
 outdir = 'temp'
 path = os.getcwd()
@@ -41,9 +43,7 @@ proxies = {
 class BaozouSpider(object):
 
     def __init__(self):
-        self.page = 1
-        self.datas = []
-        self.dic = collections.OrderedDict()
+        pass
 
     def retrieve_page(self, cur_page):
         url = "https://movie.douban.com/top250?start=%s&filter=" % (
@@ -56,107 +56,31 @@ class BaozouSpider(object):
             print("Error happens! Please check your requests.")
         return soup
 
-    def get_rank(self, soup):
-        temp = soup.select(".pic em")
-        rank = [x.string for x in temp]
-        return rank
-
-    def get_name(self, soup):
-        temp = soup.select(".hd")
-        name = []
-        for x in temp:
-            lines = x.select("a span")
-            t = ''.join(re.sub(r'\s+', ' ', s.string) for s in lines)
-            name.append(t)
-        return name
-
-    def get_rating(self, soup):
-        temp = soup.select(".rating_num")
-        rating = [x.string for x in temp]
-        return rating
-
-    def get_reviewnum(self, soup):
-        temp = soup.select(".star span")
-        reviewnum = [temp[i].string for i in range(
-            len(temp)) if (i + 1) % 4 == 0]
-        return reviewnum
-
-    def get_summary(self, soup):
-        #  Get rid of QR Code
-        temp = soup.select('.bd')[1:]
-        summary = []
-        for x in temp:
-            s = x.findAll("p", {"class": ""})
-            lines = "".join(
-                [row + "   " for row in s[0].stripped_strings]).strip()
-            summary.append(lines)
-        return summary
-
-    def get_comment(self, soup):
-        #  Get rid of QR Code
-        temp = soup.select('.bd')[1:]
-        comment = []
-        for x in temp:
-            s = x.select('.inq')
-            comment += [s[0].string] if s else [""]
-        return comment
-
-    def get_address(self, soup):
-        temp = soup.select(".pic a")
-        address = [x['href'] for x in temp]
-        return address
-
     def get_imgurl(self, soup):
-        temp = soup.select(".pic a img")
-        imgurl = [x['src'] for x in temp]
+        temp = soup.findAll('img', src=True, style="width:460px;")
+        imgurl = [img['src'] for img in temp]
         return imgurl
 
-    def retrieve_content(self, soup, dic):
-        rank = self.get_rank(soup)
-        name = self.get_name(soup)
-        rating = self.get_rating(soup)
-        reviewnum = self.get_reviewnum(soup)
-        address = self.get_address(soup)
+    def retrieve_img(self, soup):
         imgurl = self.get_imgurl(soup)
-        summary = self.get_summary(soup)
-        comment = self.get_comment(soup)
-        count = len(name)
-        for i in range(count):
-            self.datas.append([rank[i], name[i], rating[i],
-                               reviewnum[i], summary[i],
-                               comment[i], address[i], imgurl[i]])
-            content = collections.OrderedDict([("Rank", rank[i]),
-                                               ("Name", name[i]),
-                                               ("Rating", rating[i]),
-                                               ("Review_Number", reviewnum[i]),
-                                               ("Summary", summary[i]),
-                                               ("Comment", comment[i]),
-                                               ("Address", address[i]),
-                                               ("Image_URL", imgurl[i])])
-            dic[rank[i]] = content
-            #  print(content)
+        for x in imgurl:
+            try:
+                imgname = ""
+                filename = path + os.sep + imgname + ".gif"
+                print(filename)
+            except Exception:
+                print("Forbidden error, step to next one.")
 
 
-for count in range(page_sum):
-    req = urllib.request.Request(
-        url=url + str(count + 1),
-        headers=headers
-    )
-    content = urllib.request.urlopen(req).read()
-    soup = bs4.BeautifulSoup(content, "lxml")
-    img_content = soup.findAll('img', src=True, style="width:460px;")
-    url_list = [img['src'] for img in img_content]
-    print("\nFile number in page %s: %s" % (req.full_url, len(url_list)))
-    for i in range(url_list.__len__()):
-        try:
-            imgurl = url_list[i]
-            imgname = str(img_startnum).zfill(4)
-            filename = path + os.sep + imgname + ".gif"
-            img_startnum += 1
-            print(filename)
-            urllib.request.urlretrieve(imgurl, filename)
-        except Exception as e:
-            print("Forbidden error, step to next one.")
+def worker():
+    global Q_share
+    while not Q_share.empty():
+        url = Q_share.get()
+        spider = BaozouSpider()
+        my_soup = spider.retrieve_page(url)
+        spider.retrieve_content(my_soup)
+        #  time.sleep(1)
+        Q_share.task_done()
 
 
 def main():
@@ -169,8 +93,7 @@ def main():
         ###############################
     """)
     print("Baozou Gif Crawler Begins...")
-    my_spider = BaozouSpider()
-    my_data = my_spider.start_spider(page_sum)
+    #  my_spider = BaozouSpider()
     print("Douban Movie Crawler Ends.\n")
 
 if __name__ == '__main__':
