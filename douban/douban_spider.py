2d13eb13fe187cd285295fb3ad076c35b1780358
keyi <yike921012@gmail.com>
2016-04-02 23:30:41 -0700

    Initial commit

--- douban/douban_spider.py
+++ douban/douban_spider.py
@@ -8,7 +8,6 @@ import re
 import requests
 import warnings
 
-import time
 
 warnings.filterwarnings("ignore")
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'
--- douban/multi_thread.py
+++ douban/multi_thread.py
@@ -5,8 +5,10 @@ import requests
 import threading
 import queue
 import bs4
+import collections
+import json
 
-_DATA = []
+my_dic = {}
 FILE_LOCK = threading.Lock()
 SHARE_Q = queue.Queue()  # 构造一个不限制大小的的队列
 _WORKER_THREAD_NUM = 10  # 设置线程的个数
@@ -27,67 +29,138 @@ proxies = {
 }
 
 
+class DouBanSpider(object):
+
+    def __init__(self):
+        self.page = 1
+        self.datas = []
+        self.dic = collections.OrderedDict()
+
+    def retrieve_page(self, cur_url):
+        try:
+            page_text = requests.get(
+                cur_url, proxies, headers=headers, timeout=5).text
+            soup = bs4.BeautifulSoup(page_text, "lxml")
+        except Exception:
+            print("Error happens! Please check your requests.")
+        return soup
+
+    def get_rank(self, soup):
+        temp = soup.select(".pic em")
+        rank = [x.string for x in temp]
+        return rank
+
+    def get_name(self, soup):
+        temp = soup.select(".hd")
+        name = []
+        for x in temp:
+            lines = x.select("a span")
+            t = ''.join(re.sub(r'\s+', ' ', s.string) for s in lines)
+            name.append(t)
+        return name
+
+    def get_rating(self, soup):
+        temp = soup.select(".rating_num")
+        rating = [x.string for x in temp]
+        return rating
+
+    def get_reviewnum(self, soup):
+        temp = soup.select(".star span")
+        reviewnum = [temp[i].string for i in range(
+            len(temp)) if (i + 1) % 4 == 0]
+        return reviewnum
+
+    def get_summary(self, soup):
+        #  Get rid of QR Code
+        temp = soup.select('.bd')[1:]
+        summary = []
+        for x in temp:
+            s = x.findAll("p", {"class": ""})
+            lines = "".join(
+                [row + "   " for row in s[0].stripped_strings]).strip()
+            summary.append(lines)
+        return summary
+
+    def get_comment(self, soup):
+        #  Get rid of QR Code
+        temp = soup.select('.bd')[1:]
+        comment = []
+        for x in temp:
+            s = x.select('.inq')
+            comment += [s[0].string] if s else [""]
+        return comment
+
+    def get_address(self, soup):
+        temp = soup.select(".pic a")
+        address = [x['href'] for x in temp]
+        return address
+
+    def get_imgurl(self, soup):
+        temp = soup.select(".pic a img")
+        imgurl = [x['src'] for x in temp]
+        return imgurl
+
+    def retrieve_content(self, soup):
+        rank = self.get_rank(soup)
+        name = self.get_name(soup)
+        rating = self.get_rating(soup)
+        reviewnum = self.get_reviewnum(soup)
+        address = self.get_address(soup)
+        imgurl = self.get_imgurl(soup)
+        summary = self.get_summary(soup)
+        comment = self.get_comment(soup)
+        count = len(name)
+        for i in range(count):
+            self.datas.append([rank[i], name[i], rating[i],
+                               reviewnum[i], summary[i],
+                               comment[i], address[i], imgurl[i]])
+            content = collections.OrderedDict([("Rank", rank[i]),
+                                               ("Name", name[i]),
+                                               ("Rating", rating[i]),
+                                               ("Review_Number", reviewnum[i]),
+                                               ("Summary", summary[i]),
+                                               ("Comment", comment[i]),
+                                               ("Address", address[i]),
+                                               ("Image_URL", imgurl[i])])
+            my_dic[rank[i]] = content
+            #  print(content)
+
+
 def worker():
     global SHARE_Q
     while not SHARE_Q.empty():
-        url = SHARE_Q.get()  # 获得任务
-        my_page = get_page(url)
-        find_title(my_page)  # 获得当前页面的电影名
+        url = SHARE_Q.get()
+        spider = DouBanSpider()
+        my_soup = spider.retrieve_page(url)
+        spider.retrieve_content(my_soup)
         #  time.sleep(1)
         SHARE_Q.task_done()
-
-
-def get_page(url):
-    try:
-        my_page = requests.get(url, proxies, headers=headers, timeout=5).text
-        soup = bs4.BeautifulSoup(my_page, "lxml")
-    except Exception:
-        print("Error happens")
-    return soup
-
-
-def get_rank(soup):
-    temp = soup.select(".pic em")
-    rank = [x.string for x in temp]
-    return rank
-
-
-def get_name(soup):
-    temp = soup.select(".hd")
-    name = []
-    for x in temp:
-        lines = x.select("a span")
-        t = ''.join(re.sub(r'\s+', ' ', s.string) for s in lines)
-        name.append(t)
-    return name
-
-
-def find_title(soup):
-    rank = get_rank(soup)
-    name = get_name(soup)
-    rt = [x + "  " + y for x, y in zip(rank, name)]
-    _DATA.append(rt)
 
 
 def main():
     global SHARE_Q
     threads = []
     douban_url = "http://movie.douban.com/top250?start={page}&filter=&type="
-    # 向队列中放入任务, 真正使用时, 应该设置为可持续的放入任务
     for index in range(10):
         SHARE_Q.put(douban_url.format(page=index * 25))
     for i in range(_WORKER_THREAD_NUM):
-        #  thread = MyThread(worker)
         thread = threading.Thread(target=worker)
-        thread.start()  # 线程开始处理任务
+        thread.start()
         threads.append(thread)
     for thread in threads:
         thread.join()
     SHARE_Q.join()
-    with open("movie.txt", "w") as my_file:
-        for page in _DATA:
-            for movie_name in page:
-                my_file.write(movie_name + "\n")
+    import time
+    st = time.time()
+    od = collections.OrderedDict(
+        sorted(my_dic.items(), key=lambda x: int(x[0])))
+    print(time.time() - st)
+    out = "output_multiThread.json"
+    raw_data = json.dumps(
+        od, indent=4, ensure_ascii=False, sort_keys=False)
+    with open(out, 'w') as f:
+        f.write(raw_data)
+    print("Data has been written to %s successfully!" % (out))
     print("Spider Successful!!!")
 
 if __name__ == '__main__':
