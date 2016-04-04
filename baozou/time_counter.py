4dfa60391beb4854fcfcd5eb1c2f594d26805cc7
keyi <yike921012@gmail.com>
2016-04-04 14:55:00 -0700

    Initial commit

--- baozou/time_counter.py
+++ baozou/time_counter.py
@@ -3,6 +3,6 @@ import timeit
 t = timeit.Timer('main()', 'from multi_thread import main')
 print(t.repeat(3, 1))
 print("Multi-thread:")
-t = timeit.Timer('main()', 'from pl import main')
+t = timeit.Timer('main()', 'from lp import main')
 print(t.repeat(3, 1))
 print("pl")
