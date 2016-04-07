d0700a0bbd4026f2cff73b00c59d21a87b76aec7
keyi <yike921012@gmail.com>
2016-04-07 14:09:18 -0700

    Initial commit

--- Spider_Projects/imdb/imdb.py
+++ Spider_Projects/imdb/imdb.py
@@ -15,7 +15,7 @@ import mparameter
 MY_DIC = collections.OrderedDict()
 PAGE_SIZE = 1
 DB_NAME = 'Movie'
-TB_NAME = 'Douban'
+TB_NAME = 'IMDB'
 OUTPUT = 'output.json'
 
 
@@ -23,6 +23,7 @@ class DoubanSpider(object):
 
     def __init__(self):
         self.url = "http://www.imdb.com/chart/top?sort=rk,asc&mode=simple&page=1"
+        self.prefix = "http://www.imdb.com"
 
     def retrieve_page(self, cur_page):
         url = self.url
@@ -42,45 +43,41 @@ class DoubanSpider(object):
             print("Error happens! Please check your requests.")
         return soup
 
-    def get_rank(self, soup):
+    def get_rny(self, soup):
         temp = soup.select(".titleColumn")
-        rank = [x.string for x in temp]
-        return rank
-
-    def get_name(self, soup):
-        temp = soup.select(".titleColumn a")
-        name = [x.string for x in temp]
-        return name
+        rank = []
+        name = []
+        year = []
+        for x in temp:
+            lines = [row for row in x.stripped_strings]
+            rank.append(lines[0][:-1])
+            name.append(lines[1])
+            year.append(lines[2][1:-1])
+        return [rank, name, year]
 
     def get_rating(self, soup):
         temp = soup.select(".imdbRating strong")
         rating = [x.string for x in temp]
         return rating
 
-    def get_year(self, soup):
-        temp = soup.select(".titleColumn span")
-        year = [x.string for x in temp]
-        return year
-
     def get_address(self, soup):
         temp = soup.select(".titleColumn a")
-        address = [x['href'] for x in temp]
+        address = [self.prefix + x['href'] for x in temp]
         return address
 
     def retrieve_content(self, soup):
         if soup != "FLAG":
-            rank = self.get_rank(soup)
-            name = self.get_name(soup)
+            rank, name, year = self.get_rny(soup)
             rating = self.get_rating(soup)
-            year = self.get_year(soup)
             address = self.get_address(soup)
             count = len(name)
             for i in range(count):
                 content = collections.OrderedDict([("Rank", rank[i]),
                                                    ("Name", name[i]),
                                                    ("Rating", rating[i]),
-                                                   ("Comment", year[i]),
+                                                   ("Year", year[i]),
                                                    ("Address", address[i])])
+                #  print(content)
                 MY_DIC[rank[i]] = content
 
 
@@ -88,22 +85,23 @@ def main():
     print("""
         ###############################
 
-             Douban Top250 Movies
+             IMDB Top250 Movies
                Author: Ke Yi
 
         ###############################
     """)
-    print("Douban Movie Crawler Begins...")
+    print("IMDB Movie Crawler Begins...")
     my_spider = DoubanSpider()
     my_soup = my_spider.retrieve_page(0)
     my_spider.retrieve_content(my_soup)
     my_file = mjson.RWfile(OUTPUT)
     my_file.write_in(MY_DIC)
-    #  my_file.read_out()  # Read results from output file
+    my_file.read_out()  # Read results from output file
     my_db = mdatabase.DB(DB_NAME, TB_NAME)
     my_db.db_insert(MY_DIC)
-    #  my_db.db_retrieval()  # Read results from mysql database
+    my_db.db_retrieval()  # Read results from mysql database
     my_db.db_close()
+    print("IMDB Movie Crawler Ends...")
 
 if __name__ == '__main__':
     main()
