140944aa856e8fac3584f070b48de72ef5462717
keyi <yike921012@gmail.com>
2016-04-06 22:50:13 -0700

    Initial commit

--- Spider_Projects/template/mjson.py
+++ Spider_Projects/template/mjson.py
@@ -2,6 +2,9 @@ import json
 import collections
 
 
+INDENT_SIZE = 4
+
+
 class RWfile(object):
 
     def __init__(self, fname):
@@ -9,7 +12,7 @@ class RWfile(object):
 
     def write_in(self, raw_datas):
         clean_datas = json.dumps(
-            raw_datas, indent=4, ensure_ascii=False, sort_keys=False)
+            raw_datas, indent=INDENT_SIZE, ensure_ascii=False, sort_keys=False)
         with open(self.fname, 'w') as f:
             f.write(clean_datas)
         print("Write data to %s" % (self.fname))
@@ -17,6 +20,7 @@ class RWfile(object):
     def read_out(self):
         with open(self.fname) as f:
             raw_datas = json.load(f, object_pairs_hook=collections.OrderedDict)
-            clean_datas = json.dumps(raw_datas, indent=4, ensure_ascii=False)
+            clean_datas = json.dumps(
+                raw_datas, indent=INDENT_SIZE, ensure_ascii=False)
         print("Read data from %s" % (self.fname))
         print(clean_datas)
