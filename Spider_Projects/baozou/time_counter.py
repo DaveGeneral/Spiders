9ca35bf8b8e88ccea9f7c129e65b820aad62f826
keyi <yike921012@gmail.com>
2016-04-06 20:49:57 -0700

    Initial commit

--- Spider_Projects/baozou/time_counter.py
+++
@@ -1,8 +0,0 @@
-import timeit
-
-t = timeit.Timer('main()', 'from lp import main')
-print(t.repeat(3, 1))
-print("pl")
-t = timeit.Timer('main()', 'from go import main')
-print(t.repeat(3, 1))
-print("go")
