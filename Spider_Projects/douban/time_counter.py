549f744f4273536fd9715fbc515659afda84285b
keyi <yike921012@gmail.com>
2016-04-06 20:36:53 -0700

    Initial commit

--- Spider_Projects/douban/time_counter.py
+++ Spider_Projects/douban/time_counter.py
@@ -1,11 +1,8 @@
 import timeit
 
-t = timeit.Timer('main()', 'from multi_thread import main')
-print(t.repeat(5, 1))
-print("Multi-thread:")
-#  t = timeit.Timer('main()', 'from ag import main')
-#  print(t.repeat(5, 1))
-#  print("Multi-thread ag:")
-t = timeit.Timer('main()', 'from pl import main')
-print(t.repeat(5, 1))
-print("Multi-thread pl:")
+t1 = timeit.Timer('main()', 'from douban import main')
+print(t1.repeat(3, 1))
+print("Normal Version:")
+t2 = timeit.Timer('main()', 'from douban_mthreads import main')
+print(t2.repeat(5, 1))
+print("Multi-threads Version:")
