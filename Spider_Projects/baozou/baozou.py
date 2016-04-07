b0a2458d404799ba1c01ddddd053f453a192d8b0
keyi <yike921012@gmail.com>
2016-04-06 21:54:23 -0700

    Initial commit

--- Spider_Projects/baozou/baozou.py
+++ Spider_Projects/baozou/baozou.py
@@ -86,7 +86,6 @@ def main():
         ###############################
 
              BaoZou Gif Crawler
-           (Multi-Process Version)
                Author: Ke Yi
 
         ###############################
