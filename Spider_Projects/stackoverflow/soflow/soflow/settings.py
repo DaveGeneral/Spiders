c170f7393a5ee6d24eb30f459cb159c2a0556ad4
keyi <yike921012@gmail.com>
2016-04-09 15:29:59 -0700

    Initial commit

---
+++ Spider_Projects/stackoverflow/soflow/soflow/settings.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for soflow project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'soflow'
+
+SPIDER_MODULES = ['soflow.spiders']
+NEWSPIDER_MODULE = 'soflow.spiders'
+
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+# CONCURRENT_REQUESTS=32
+
+# Configure a delay for requests for the same website (default: 0)
+# DOWNLOAD_DELAY=3
+# The download delay setting will honor only one of:
+# CONCURRENT_REQUESTS_PER_DOMAIN=16
+# CONCURRENT_REQUESTS_PER_IP=16
+
+# Disable cookies (enabled by default)
+# COOKIES_ENABLED=False
+
+# Disable Telnet Console (enabled by default)
+# TELNETCONSOLE_ENABLED=False
+
+# Override the default request headers:
+# DEFAULT_REQUEST_HEADERS = {
+#   'Accept': ('text/html,application/xhtml+xml,'
+#              'application/xml;q=0.9,*/*;q=0.8'),
+#   'Accept-Language': 'en',
+# }
+
+# Enable or disable spider middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+# SPIDER_MIDDLEWARES = {
+#    'soflow.middlewares.MyCustomSpiderMiddleware': 543,
+# }
+
+# Enable or disable downloader middlewares
+# DOWNLOADER_MIDDLEWARES = {
+#    'soflow.middlewares.MyCustomDownloaderMiddleware': 543,
+# }
+
+# Enable or disable extensions
+# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
+# EXTENSIONS = {
+#    'scrapy.telnet.TelnetConsole': None,
+# }
+
+# Configure item pipelines
+# See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
+# ITEM_PIPELINES = {
+#    'soflow.pipelines.SomePipeline': 300,
+# }
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# AUTOTHROTTLE_ENABLED=True
+# The initial download delay
+# AUTOTHROTTLE_START_DELAY=5
+# The maximum download delay to be set in case of high latencies
+# AUTOTHROTTLE_MAX_DELAY=60
+# Enable showing throttling stats for every response received:
+# AUTOTHROTTLE_DEBUG=False
+
+# Enable and configure HTTP caching (disabled by default)
+# HTTPCACHE_ENABLED=True
+# HTTPCACHE_EXPIRATION_SECS=0
+# HTTPCACHE_DIR='httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES=[]
+# HTTPCACHE_STORAGE='scrapy.extensions.httpcache.FilesystemCacheStorage'
