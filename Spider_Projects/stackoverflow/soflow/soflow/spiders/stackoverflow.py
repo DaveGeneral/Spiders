7c7d4cbdb0dd58b7bd1b9faecbb336800dfcb531
keyi <yike921012@gmail.com>
2016-04-09 18:49:38 -0700

    Initial commit

--- Spider_Projects/stackoverflow/soflow/soflow/spiders/stackoverflow.py
+++ Spider_Projects/stackoverflow/soflow/soflow/spiders/stackoverflow.py
@@ -1,15 +1,17 @@
-from scrapy import Spider
-from scrapy.selector import Selector
 from soflow.items import SoflowItem
+from scrapy.spider import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 
-class StackSpider(Spider):
+class StackSpider(CrawlSpider):
     name = 'stack'
     allowed_domains = ['stackoverflow.com']
     start_urls = ['http://stackoverflow.com/questions?sort=newest']
+    rules = [Rule(LinkExtractor(allow=r'questions\?page=[0-5]&sort=newest'),
+                  callback='parse_item')]
 
-    def parse(self, response):
-        blocks = Selector(response).xpath('//div[@class="summary"]/h3')
+    def parse_item(self, response):
+        blocks = response.xpath('//div[@class="summary"]/h3')
         for question in blocks:
             item = SoflowItem()
             item['title'] = question.xpath(
