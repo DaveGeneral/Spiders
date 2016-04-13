75b0d6a2479902f1f279f4b1ed138b80f5194d4a
keyi <yike921012@gmail.com>
2016-04-13 11:47:43 -0700

    Initial commit

---
+++ Spider_Projects/bdmi/genre_mthread.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import bs4
+import collections
+import queue
+import threading
+import re
+import requests
+import sys
+sys.path.append('../template')
+import mdatabase
+import mjson
+import mparameter
+
+
+GENRE_LIST = [{'name': 'Action', 'order': '1', 'page_size': 20},
+              {'name': 'Adventure', 'order': '2', 'page_size': 16},
+              {'name': 'Animation', 'order': '3', 'page_size': 5},
+              {'name': 'Biography', 'order': '4', 'page_size': 5},
+              {'name': 'Comedy', 'order': '5', 'page_size': 28},
+              {'name': 'Crime', 'order': '6', 'page_size': 16},
+              {'name': 'Drama', 'order': '7', 'page_size': 38},
+              {'name': 'Family', 'order': '8', 'page_size': 8},
+              {'name': 'Fantasy', 'order': '9', 'page_size': 11},
+              {'name': 'Film_Noir', 'order': '10', 'page_size': 1},
+              {'name': 'History', 'order': '11', 'page_size': 3},
+              {'name': 'Horror', 'order': '12', 'page_size': 9},
+              {'name': 'Music', 'order': '13', 'page_size': 3},
+              {'name': 'Musical', 'order': '14', 'page_size': 2},
+              {'name': 'Mystery', 'order': '15', 'page_size': 10},
+              {'name': 'Romance', 'order': '16', 'page_size': 16},
+              {'name': 'Sci_Fi', 'order': '17', 'page_size': 11},
+              {'name': 'Sport', 'order': '18', 'page_size': 3},
+              {'name': 'Thriller', 'order': '19', 'page_size': 25},
+              {'name': 'War', 'order': '20', 'page_size': 3},
+              {'name': 'Western', 'order': '21', 'page_size': 2}]
+
+DB_NAME = 'Movie'
+Thread_NUM = 10  # thread number
+Q_SHARE = queue.Queue()
+
+
+class GenreSpider(object):
+
+    def __init__(self):
+        self.url = ("http://www.imdb.com/search/title?genres=%s"
+                    "&num_votes=25000,&pf_rd_i=top&pf_rd_m=A2FGELUUNOQJNL"
+                    "&pf_rd_p=2406822102&pf_rd_r=09E81XJGYBWBJZTG7WYJ"
+                    "&pf_rd_s=right-6&pf_rd_t=15506&ref_=chttp_gnr_%s"
+                    "&sort=user_rating,desc&start=%s"
+                    "&title_type=feature")
+        self.prefix = "http://www.imdb.com"
+
+    def retrieve_page(self, cur_name, cur_order, cur_page):
+        url = self.url % (cur_name.lower(), cur_order, str(cur_page * 50 + 1))
+        pm = mparameter.Parameter()
+        headers = pm.get_headers()
+        proxies = pm.get_proxies()
+        soup = "FLAG"
+        try:
+            response = requests.get(
+                url, proxies, headers=headers, timeout=5)
+            status = response.status_code
+            if status == 200:
+                soup = bs4.BeautifulSoup(response.text, "lxml")
+            else:
+                print("%s error to reach the server %s" % (status, url))
+        except Exception:
+            print("Error happens! Please check your requests.")
+        return soup
+
+    def get_rank(self, soup):
+        temp = soup.select(".detailed .number")
+        rank = [x.string[:-1] for x in temp]
+        return rank
+
+    def get_rating(self, soup):
+        temp = soup.select(".rating-rating")
+        rating = []
+        for x in temp:
+            lines = [row for row in x.stripped_strings]
+            rating.append("".join(lines))
+        return rating
+
+    def get_nameaddress(self, soup):
+        temp = soup.select(".image a")
+        name = []
+        address = []
+        for x in temp:
+            name.append(x['title'][:-7])
+            address.append(self.prefix + x['href'])
+        return [name, address]
+
+    def get_outline(self, soup):
+        temp = soup.select(".outline")
+        outline = []
+        for x in temp:
+            lines = [row for row in x.stripped_strings]
+            outline.append(" ".join(lines))
+        return outline
+
+    def get_credit(self, soup):
+        temp = soup.select(".credit")
+        credit = []
+        for x in temp:
+            lines = [row for row in x.stripped_strings]
+            credit.append(re.sub(r"\s,", ",", " ".join(lines)))
+        return credit
+
+    def get_genre(self, soup):
+        temp = soup.select(".genre")
+        genre = []
+        for x in temp:
+            lines = " | ".join([s.string for s in x.select("a")])
+            genre.append(lines)
+        return genre
+
+    def get_year(self, soup):
+        temp = soup.select(".year_type")
+        year = [x.string[1:-1] for x in temp]
+        return year
+
+    def get_runtime(self, soup):
+        temp = soup.select(".runtime")
+        runtime = [x.string[:-1] for x in temp]
+        return runtime
+
+    def get_certificate(self, soup):
+        temp = soup.select(".certificate")
+        certificate = []
+        for x in temp:
+            s = x.select("span")
+            certificate += [s[0]['title']] if s else [""]
+        return certificate
+
+    def retrieve_content(self, soup, DIC):
+        global MY_DIC
+        if soup != "FLAG":
+            rank = self.get_rank(soup)
+            name, address = self.get_nameaddress(soup)
+            rating = self.get_rating(soup)
+            year = self.get_year(soup)
+            outline = self.get_outline(soup)
+            credit = self.get_credit(soup)
+            genre = self.get_genre(soup)
+            runtime = self.get_runtime(soup)
+            certificate = self.get_certificate(soup)
+            count = len(rank)
+            for i in range(count):
+                content = collections.OrderedDict([("Rank", rank[i]),
+                                                   ("Name", name[i]),
+                                                   ("Rating", rating[i]),
+                                                   ("Year", year[i]),
+                                                   ("Certificate",
+                                                    certificate[i]),
+                                                   ("Runtime", runtime[i]),
+                                                   ("Genre", genre[i]),
+                                                   ("Credit", credit[i]),
+                                                   ("Address", address[i]),
+                                                   ("Outline", outline[i])])
+                #  print(content)
+                DIC[rank[i]] = content
+
+
+class Workers(threading.Thread):
+
+    def __init__(self, item):
+        threading.Thread.__init__(self)
+        self.item = item
+
+    def run(self):
+        while True:
+            MY_DIC = collections.OrderedDict()
+            item = self.item.get()
+            for i in range(item['page_size']):
+                my_spider = GenreSpider()
+                my_soup = my_spider.retrieve_page(
+                    item['name'], item['order'], i)
+                my_spider.retrieve_content(my_soup, MY_DIC)
+            ol = sorted(MY_DIC.items(), key=lambda x: int(x[0]))
+            ol = [s[1] for s in ol]
+            my_file = mjson.RWfile(item['name'].lower() + '.json')
+            my_file.write_in(ol)
+            #  my_file.read_out()  # Read results from output file
+            my_db = mdatabase.DB(DB_NAME, item['name'])
+            my_db.db_insert(ol)
+            #  my_db.db_retrieval()  # Read results from mysql database
+            my_db.db_close()
+            self.item.task_done()
+
+
+def main():
+    print("""
+        ###############################
+
+           IMDB Mutiple Genre Movies
+             Multi-Threads Verison
+                Author: Ke Yi
+
+        ###############################
+    """)
+    print("IMDB Mutiple Genre Crawler Begins...")
+    for i in range(Thread_NUM):
+        thread = Workers(Q_SHARE)
+        thread.daemon = True
+        thread.start()
+    for x in GENRE_LIST:
+        Q_SHARE.put(x)
+    Q_SHARE.join()
+    print("IMDB Mutiple Genre Crawler Ends...")
+
+if __name__ == '__main__':
+    main()
---
+++ Spider_Projects/bdmi/imdb.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import bs4
+import collections
+import requests
+import sys
+sys.path.append('../template')
+import mdatabase
+import mjson
+import mparameter
+
+
+MY_DIC = collections.OrderedDict()
+PAGE_SIZE = 1
+DB_NAME = 'Movie'
+TB_NAME = 'IMDB'
+OUTPUT = 'top250.json'
+
+
+class IMDBSpider(object):
+
+    def __init__(self):
+        self.url = ("http://www.imdb.com/chart/top?"
+                    "sort=rk,asc&mode=simple&page=1")
+        self.prefix = "http://www.imdb.com"
+
+    def retrieve_page(self, cur_page):
+        url = self.url
+        pm = mparameter.Parameter()
+        headers = pm.get_headers()
+        proxies = pm.get_proxies()
+        soup = "FLAG"
+        try:
+            response = requests.get(
+                url, proxies, headers=headers, timeout=5)
+            status = response.status_code
+            if status == 200:
+                soup = bs4.BeautifulSoup(response.text, "lxml")
+            else:
+                print("%s error to reach the server %s" % (status, url))
+        except Exception:
+            print("Error happens! Please check your requests.")
+        return soup
+
+    def get_rny(self, soup):
+        temp = soup.select(".titleColumn")
+        rank = []
+        name = []
+        year = []
+        for x in temp:
+            lines = [row for row in x.stripped_strings]
+            rank.append(lines[0][:-1])
+            name.append(lines[1])
+            year.append(lines[2][1:-1])
+        return [rank, name, year]
+
+    def get_rating(self, soup):
+        temp = soup.select(".imdbRating strong")
+        rating = [x.string for x in temp]
+        return rating
+
+    def get_address(self, soup):
+        temp = soup.select(".titleColumn a")
+        address = [self.prefix + x['href'] for x in temp]
+        return address
+
+    def retrieve_content(self, soup):
+        if soup != "FLAG":
+            rank, name, year = self.get_rny(soup)
+            rating = self.get_rating(soup)
+            address = self.get_address(soup)
+            count = len(name)
+            for i in range(count):
+                content = collections.OrderedDict([("Rank", rank[i]),
+                                                   ("Name", name[i]),
+                                                   ("Rating", rating[i]),
+                                                   ("Year", year[i]),
+                                                   ("Address", address[i])])
+                #  print(content)
+                MY_DIC[rank[i]] = content
+
+
+def main():
+    print("""
+        ###############################
+
+             IMDB Top250 Movies
+               Author: Ke Yi
+
+        ###############################
+    """)
+    print("IMDB Movie Crawler Begins...")
+    my_spider = IMDBSpider()
+    my_soup = my_spider.retrieve_page(0)
+    my_spider.retrieve_content(my_soup)
+    print("IMDB Movie Crawler Ends...")
+    ol = sorted(MY_DIC.items(), key=lambda x: int(x[0]))  # ordered list
+    ol = [s[1] for s in ol]
+    my_file = mjson.RWfile(OUTPUT)
+    my_file.write_in(ol)
+    #  my_file.read_out()  # Read results from output file
+    my_db = mdatabase.DB(DB_NAME, TB_NAME)
+    my_db.db_insert(ol)
+    #  my_db.db_retrieval()  # Read results from mysql database
+    my_db.db_close()
+
+if __name__ == '__main__':
+    main()
