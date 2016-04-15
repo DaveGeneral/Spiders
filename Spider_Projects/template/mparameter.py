32685abe2be7c6c90c270d1abe0157c6ccfa8335
keyi <yike921012@gmail.com>
2016-04-15 14:36:04 -0700

    Initial commit

--- Spider_Projects/template/mparameter.py
+++ Spider_Projects/template/mparameter.py
@@ -22,7 +22,7 @@ class Parameter(object):
                    'Accept': 'text/html,application/xhtml+xml,'
                    'application/xml;q=0.9,*/*;q=0.8',
                    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-                   'Accept-Encoding': 'none',
+                   'Accept-Encoding': 'gzip,deflate',
                    'Accept-Language': 'en-US,en;q=0.8',
                    'Connection': 'keep-alive'}
         return headers
